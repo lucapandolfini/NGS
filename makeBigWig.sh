117c79f5167acdef3ee221d546adf403f4cd8893
Luca Pandolfini <36173642+lucapandolfini@users.noreply.github.com>
2019-02-22 17:16:56 +0000

    Add files via upload

--- makeBigWig.sh
+++ makeBigWig.sh
@@ -3,7 +3,8 @@
 red='\033[0;31m'
 nc='\033[0m'
 
-FILE=$1
+CHR_SIZE=$1
+FILE=$2
 NAME="${FILE/.bam/}"
 
 echo -e "${red}\n"
@@ -13,7 +14,6 @@ echo -e "${red}----------------------------------------------${nc}"
 
 scale_pos=$(samtools view -F 256 -c $NAME.bam)
 scale_pos=$(echo "1000000/$scale_pos" | bc -l)
-genomeCoverageBed -split -ibam $NAME.bam -bg -g ~/LAB/ANNOTATION/hg38.chrom.sizes -trackline -trackopts "speciesOrder=hg38 db=hg38 visibility=full color=45,45,201 name=$NAME.Coverage description=$NAME-Coverage" -scale $scale_pos > $NAME.cov
-bedGraphToBigWig $NAME.cov ~/LAB/ANNOTATION/hg38.chrom.sizes $NAME.bw
+genomeCoverageBed -split -ibam $NAME.bam -bg -g $CHR_SIZE -trackline -trackopts "speciesOrder=hg38 db=hg38 visibility=full color=45,45,201 name=$NAME.Coverage description=$NAME-Coverage" -scale $scale_pos > $NAME.cov
+bedGraphToBigWig $NAME.cov $CHR_SIZE $NAME.bw
 rm $NAME.cov
-
